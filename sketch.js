7f10f6a6dfd51e1e4f10f6c51168705d5a05d50d
iphone513s <43226563+iphone513s@users.noreply.github.com>
2025-05-23 10:32:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,13 +8,18 @@ const points = [
   76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184
 ];
 
+// 新增要連接的點的陣列
+const extraPoints = [
+  76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184
+];
+
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 
-  facemesh = ml5.facemesh(video, modelReady);
+  facemesh = ml5.faceMesh(video, modelReady);
   facemesh.on('predict', gotResults);
 }
 
@@ -35,10 +40,19 @@ function draw() {
     stroke(255, 0, 0); // 紅色
     strokeWeight(15);
 
-    // 依序連接所有指定的點
+    // 依序連接原本的 points 陣列
     for (let i = 0; i < points.length - 1; i++) {
       const pt1 = keypoints[points[i]];
       const pt2 = keypoints[points[i + 1]];
+      if (pt1 && pt2) {
+        line(pt1[0], pt1[1], pt2[0], pt2[1]);
+      }
+    }
+
+    // 依序連接新的 extraPoints 陣列
+    for (let i = 0; i < extraPoints.length - 1; i++) {
+      const pt1 = keypoints[extraPoints[i]];
+      const pt2 = keypoints[extraPoints[i + 1]];
       if (pt1 && pt2) {
         line(pt1[0], pt1[1], pt2[0], pt2[1]);
       }
